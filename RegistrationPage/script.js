bcdc5609337268c9708a3b3dc635fd3e8bc9d799
unknown <lukmanmauwal@gmail.com>
2025-02-05 10:12:04 +0100

    my project

---
+++ RegistrationPage/script.js
@@ -0,0 +1,40 @@
+document.querySelector(".bu").addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log(" am hereeeeee")
+    document.querySelector(".hu-drop-down").style.display="flex";
+    document.querySelector(".hu-drop-down").style.position="absolute";
+    document.querySelector(".hu-drop-down").style.padding="20px";
+    document.querySelector(".hu-drop-down").style.background="rgb(181, 161, 161)";
+    document.querySelector(".hu-drop-down").style.left="60px";
+    document.querySelector(".hu-drop-down").style.top="50px";
+})
+
+document.querySelector(".pdf").addEventListener("click", (e) => {
+   e.preventDefault();
+    document.querySelector(".hu2-drop-down").style.display="flex";
+    document.querySelector(".hu2-drop-down").style.position="absolute";
+    document.querySelector(".hu2-drop-down").style.padding="20px";
+    document.querySelector(".hu2-drop-down").style.background="rgb(181, 161, 161)";
+    document.querySelector(".hu2-drop-down").style.left="60px";
+    document.querySelector(".hu2-drop-down").style.top="50px";
+})
+
+document.querySelector(".mar").addEventListener("click", (e) => {
+    e.preventDefault();
+     document.querySelector(".hu3-drop-down").style.display="flex";
+     document.querySelector(".hu3-drop-down").style.position="absolute";
+     document.querySelector(".hu3-drop-down").style.padding="20px";
+     document.querySelector(".hu3-drop-down").style.background="rgb(181, 161, 161)";
+     document.querySelector(".hu3-drop-down").style.left="60px";
+     document.querySelector(".hu3-drop-down").style.top="50px";
+ })
+
+ document.querySelector(".sup").addEventListener("click", (e) => {
+    e.preventDefault();
+     document.querySelector(".hu4-drop-down").style.display="flex";
+     document.querySelector(".hu4-drop-down").style.position="absolute";
+     document.querySelector(".hu4-drop-down").style.padding="20px";
+     document.querySelector(".hu4-drop-down").style.background="rgb(181, 161, 161)";
+     document.querySelector(".hu4-drop-down").style.left="60px";
+     document.querySelector(".hu4-drop-down").style.top="50px";
+ })
